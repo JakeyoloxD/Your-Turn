74294484b66e9c29500aa5c95f89703802e397eb
JakeyoloxD <148301003+JakeyoloxD@users.noreply.github.com>
2024-08-07 16:06:52 +1000

    Patching broken abilities

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ let cardAbilities = ["Draw a card", "Create a treasure token", "Create 3 food to
     "Exchange control of target permanent you control and target permanent an opponent controls.",
     "Each player sacrifices a permanent at random.",
     "Each player exiles a card from their hand. At the beginning of the next end step, they put that card back into their hand.",
-    "Each player reveals the top card of their library. The player who reveals the card with the highest mana value puts that card into their hand and loses 5 life. You may repeat this process.",
+    "Each player reveals the top card of their library. The player who reveals the card with the highest mana value puts that card into their hand and loses 5 life.",
     "Each player exiles all creatures they control, then returns them to the battlefield under a random player's control.",
     "At the beginning of each player's upkeep, they exile the top card of their library. They may play that card this turn.",
     "Each player exchanges their hand with the player to their left.",
@@ -102,7 +102,7 @@ let cardAbilities = ["Draw a card", "Create a treasure token", "Create 3 food to
     "Each player gains control of all permanents they own. Then, each player loses life equal to the number of permanents they control.",
     "Each player discards their hand, then draws cards equal to the greatest number of cards a player discarded this way.",
     "Each player sacrifices all creatures they control, then creates that many 2/2 black Zombie creature tokens.",
-    "Each player chooses a number between 1 and 5. Each player takes damage equal to their chosen number times 3. The player who chose the lowest number draws three cards.",
+    "Each player chooses a number between 1 and 5. Each player takes damage equal to their chosen number times 3. The player or players who chose the highest number, may draw three cards.",
     "Each player flips a coin until they lose a flip. For each flip they win, they draw a card. For each flip they lose, they discard a card.",
     "Each player exchanges their library with another player's chosen at random. Each player shuffles their new library."
 ];
